07705f810f3a10fe00d76730ab2a5910ad49873f
santanaNogueira <jose.nogueira@eyenov.com>
2022-10-21 14:57:41 +0100

    Add "WebPlayer.html" and change `ViewController` to load the player from it

--- ios-webplayer-example/ViewController.swift
+++ ios-webplayer-example/ViewController.swift
@@ -14,9 +14,6 @@ class ViewController: UIViewController {
     // MARK: - UI Properties
     private var webPlayer: WebPlayer!
     
-    // MARK: - Properties
-    private var testStreamID = "5ddb98f5e4b0937e6a4507f2"
-    
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +24,7 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        webPlayer.load(streamID: testStreamID)
+        loadTestStreamFromLocalHTMLFile()
     }
 }
 
@@ -44,5 +41,19 @@ extension ViewController {
             webPlayer.bottomAnchor.constraint(equalTo: margins.bottomAnchor),
             webPlayer.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+}
+
+// MARK: - Load Stream
+extension ViewController {
+    private func loadTestStreamFromStreamID() {
+        let testStreamID = "5ddb98f5e4b0937e6a4507f2"
+        webPlayer.load(streamID: testStreamID)
+    }
+    
+    private func loadTestStreamFromLocalHTMLFile() {
+        let url = Bundle.main.url(forResource: "WebPlayer", withExtension: "html")!
+        let request = URLRequest(url: url)
+        webPlayer.load(request)
     }
 }
